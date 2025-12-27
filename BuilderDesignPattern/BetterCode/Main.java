e9d2c0d3b1a8b78ea870f8dccaa93d3c30c20c27
h1575922 <digvijay.gupta@hy-vee.com>
2025-12-27 21:31:34 +0530

    added builder pattern code enhanced

--- BuilderDesignPattern/BetterCode/Main.java
+++ BuilderDesignPattern/BetterCode/Main.java
@@ -2,12 +2,10 @@ package LLD.BuilderDesignPattern.BetterCode;
 
 public class Main {
     public static void main(String[] args) {
-        Builder builder = new Builder();
-        builder.setName("Laptop");
-        builder.setDescription("High performance laptop");
-        builder.setPrice(1500.00);
-        // set other optional fields as needed
-
-        Product product = new Product(builder);
+        Product product = Product.getBuilder()
+                .setName("Laptop")
+                .setDescription("High performance laptop")
+                .setPrice(1500.00)
+                .build();
     }
 }
--- BuilderDesignPattern/BetterCode/Product.java
+++ BuilderDesignPattern/BetterCode/Product.java
@@ -13,12 +13,69 @@ public class Product {
     private String brand;
     private ArrayList<String> images;
 
-    Product(Builder b) {
+    private Product(Builder b) {
         this.name = b.getName();
         this.description = b.getDescription();
 
         if(b.getPrice() > 0){
             this.price = b.getPrice();
         }
+    }
+
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String name;
+        private String description;
+        private double price;
+        private int discount;
+        private String category;
+        private String createdAt;
+        private String updatedAt;
+        private String brand;
+        private ArrayList<String> images;
+
+
+        Builder() {
+
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setPrice(double price) {
+            this.price = price;
+            return this;
+        }
+
+        // ..setters
+        // getters
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+
     }
 }
